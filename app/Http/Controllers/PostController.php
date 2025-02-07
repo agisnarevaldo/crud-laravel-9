b80c28692cbe5372291b25faf95702552fa5d6fe
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-02-07 22:03:18 +0700

    Add post creation functionality in PostController
    Implemented methods for viewing the create form, storing new posts, and rendering the index view. Includes validation, image upload handling, and redirection with success feedback.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         // get posts
@@ -15,5 +19,39 @@ class PostController extends Controller
 
         // render view with posts
         return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * create
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        //validate form
+        $this->validate(request(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // create post
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->input('title'),
+            'content' => $request->input('content')
+        ]);
+
+        // redirect to index
+        return redirect() -> route('posts.index')->with('success', 'Post created successfully.');
     }
 }
